bcda2557fe6a2fc44a8f3f49824620d59be423c1
OLWASI LINDA <159750524+Olwasi@users.noreply.github.com>
2024-04-13 19:58:29 +0300

    Update logic2.c

--- logic2.c
+++ logic2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 /*OLWASI LINDA ENE212-0146/2021
     JOSHUA KWEMBOI ENE212-0088/2021
     FEDLEY SIKOLIA ENE212-0183/2021
     SOSPETER NG'ANG'A
-    BRIAN OMARIBA */
-#include <stdlib.h>
+    BRIAN OMARIBA  ENE212-0162/2021*/
+
 #define TRUE 1
 #define FALSE 0
 
-// Define boolean logic functions
+
 int AND(int a, int b) {
     return a && b;
 }
@@ -36,12 +38,12 @@ int XNOR(int a, int b){
     return !(a^b);
 }
 
-// Function to generate truth table for a given logic function
+
 void generateTruthTable(int (*logicFunc)(int, int), char *gateName, int inputA, int inputB) {
     printf("Truth table for %s gate:\n", gateName);
     printf("Input A | Input B | Output\n");
     printf("--------+---------+--------\n");
-// Loop through all possible input combinations
+
     for (int a = 0; a <= 1; a++) {
         for (int b = 0; b <= 1; b++) {
             printf("   %d    |    %d    |   %d\n", a, b, logicFunc(a, b));
@@ -51,8 +53,7 @@ void generateTruthTable(int (*logicFunc)(int, int), char *gateName, int inputA, 
 
 int main() {
     int choice, inputA, inputB;
-
-    // Prompt user to select gate
+    
     printf("Select gate:\n");
     printf("1. AND\n");
     printf("2. OR\n");
@@ -64,8 +65,7 @@ int main() {
     printf("Enter your choice: ");
     scanf("%d", &choice);
     
-case 4:
-            // Prompt user for inputs A and B for NOR gate
+       case 4:
             printf("Enter input value for A (0 or 1): ");
             scanf("%d", &inputA);
             printf("Enter input value for B (0 or 1): ");
@@ -74,7 +74,6 @@ case 4:
             generateTruthTable(NOR, "NOR", inputA, inputB);
             break;
         case 5:
-            // Prompt user for inputs A and B for NAND gate
             printf("Enter input value for A (0 or 1): ");
             scanf("%d", &inputA);
             printf("Enter input value for B (0 or 1): ");
